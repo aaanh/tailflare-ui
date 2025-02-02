e2b6e6fefabcff0fe44cfb500434cbb500d2a12e
Anh Nguyen <anhnguyen@aaanh.com>
2025-02-01 21:29:35 -0500

    wip

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,17 @@
-import Image from "next/image";
+import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert";
 
 export default function Home() {
-  return <div></div>;
+  return (
+    <div>
+      <Alert className="bg-blue-400 shadow-md m-4 w-fit text-lg">
+        {/* <DatabaseIcon className="w-5 h-5" /> */}
+        <AlertTitle className="font-bold">Just wanna let u know!</AlertTitle>
+        <AlertDescription>
+          The app is designed to make all API requests from the client side
+          (from your local machine directly to Tailscale and Cloudflare
+          endpoints), your keys and data are never seen by me nor my server.
+        </AlertDescription>
+      </Alert>
+    </div>
+  );
 }
--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,11 +1,22 @@
+import Image from "next/image";
+
 export default function Header() {
   return (
-    <div className="flex items-center gap-2 p-2 border-b">
-      <div className="relative w-8 h-8">
-        <div className="top-1.5 left-3 absolute bg-blue-500/90 border rounded-full w-5 h-5"></div>
-        <div className="top-1.5 left-1 absolute bg-orange-500/90 border rounded-full w-5 h-5"></div>
+    <div className="flex items-center gap-2 shadow p-2 border-b">
+      <Image
+        src="/aaanh.webp"
+        width={32}
+        height={32}
+        alt="logo"
+        className="rotate-45"
+      />
+      <div className="relative">
+        <h1 className="z-50 font-bold text-2xl">Tailflare</h1>
+        <span className="top-2.5 -left-1 -z-10 absolute bg-yellow-500/80 skew-x-6 w-[8.5rem] h-4 -rotate-2"></span>
       </div>
-      <h1 className="text-2xl">Tailflare</h1>
+      <div className="p-2 border-black border-l-2">
+        <p>Sync Tailscale hosts to Cloudflare DNS</p>
+      </div>
     </div>
   );
 }
